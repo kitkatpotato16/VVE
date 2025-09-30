7ecaa345288165b911e0d9a6694618861aaf2a13
kitkatpotato16 <523488@vistacollege.nl>
2025-09-30 13:25:20 +0200

    h

--- php/config.php
+++ php/config.php
@@ -1,13 +1,4 @@
 <?php
-$host = "localhost";
-$dbname = "vve_db";
-$username = "root";
-$password = ""; // gebruik je eigen wachtwoord als je die hebt ingesteld
 
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Verbinding mislukt: " . $e->getMessage());
-}
-?>
+$host = "localhost";
+$ 
